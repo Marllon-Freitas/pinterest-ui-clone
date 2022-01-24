42c1ab749487b5300f415e5da51676d33e7bd946
Marllon-Freitas <marllonfreitas64@gmail.com>
2022-01-24 09:16:51 -0300

    updating the README file

--- src/App.js
+++ src/App.js
@@ -17,31 +17,6 @@ import { GlobalStyle } from "./style/GlobalStyle";
 function App() {
   const [pins, setPins] = useState([ ]);
  
-  const getNewPins = () => {
-    let promises = [];
-    let pinData = [];
-
-    let firstPins = ["gato", "cachorro", "aesthetic", "paisagem", "oceano", "ilhas", "morte", "jogos"];
-
-    firstPins.forEach((pinTherm) => {
-      promises.push(
-        getImages(pinTherm).then((res) => {
-          let results = res.data.results;
-          pinData = pinData.concat(results);
-          pinData.sort(function(a, b) {
-            return 0.5 - Math.random();
-          })
-        })
-      )
-      Promise.all(promises).then(() => {
-        setPins(pinData);
-      })
-    })
-  }
-  useEffect(() => {
-    getNewPins()
-  });
-
   const getImages = (searchTerm) => {
     return unsplash.get("https://api.unsplash.com/search/photos", {
       params: {
